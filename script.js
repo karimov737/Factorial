0ad8d11fb5b228aa84acc7091b0a1b4f6b8eb99a
Bobirjon <karimovbobir24@gmail.com>
2024-03-16 12:06:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let factorial = 1;
+console.log("Kritilgan sonlar :",);
+for (let i = +prompt("son :"); i > 1; i--) {
+    factorial *= i;
+    // console.log("Kritilgan sonlar :", factorial);
+}
+// console.log("Kritilgan sonlar :", factorial);
+alert(factorial);
+console.log(factorial);
